59212ec7cc79138c78901e8f0f557909a6c6b3d5
Joshua Lai <jdlai@cpp.edu>
2021-09-02 15:08:29 -0700

    added interface methods

--- ResizableArrayBag.java
+++ ResizableArrayBag.java
@@ -1,5 +1,9 @@
-public class ResizableArrayBag implements BagInterface
+public class ResizableArrayBag implements BagInterface<Object>
 {
+   public ResizableArrayBag()
+   {
+      
+   }
    @Override
    public void union()
    {
@@ -14,5 +18,50 @@ public class ResizableArrayBag implements BagInterface
    public void difference()
    {
       
+   }
+   @Override
+   public int getCurrentSize()
+   {
+      return 0;
+   }
+   @Override
+   public boolean isEmpty()
+   {
+      return false;
+   }
+   @Override
+   public boolean add(Object newEntry)
+   {
+      return false;
+   }
+   @Override
+   public Object remove()
+   {
+      return null;
+   }
+   @Override
+   public boolean remove(Object anEntry)
+   {
+      return false;
+   }
+   @Override
+   public void clear()
+   {
+      
+   }
+   @Override
+   public int getFrequencyOf(Object anEntry)
+   {
+      return 0;
+   }
+   @Override
+   public boolean contains(Object anEntry)
+   {
+      return false;
+   }
+   @Override
+   public Object[] toArray()
+   {
+      return null;
    }
 }
